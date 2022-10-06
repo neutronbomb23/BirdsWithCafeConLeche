959badac29e1afeda5061d9ebfcdd3f15c67c453
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2022-10-06 10:23:21 +0200

    cambiado el main.js

--- Birds with café con leche/src/main.js
+++ Birds with café con leche/src/main.js
@@ -1,32 +1,28 @@
 import Firstscene from './scenes/Firstscene.js'
 
-const config = {
-
-	title: 'Birds with café con leche',
-	url: '',
-
-
-	pixelArt: true;
-
+var config = {
 	type: Phaser.AUTO,
-	width: 640,
-	height: 360,
-	parent: 'container',
-	backgroundColor: #34495E
-
-	banner:{
-		hidePhaser:true,
-		text: '#000000',
-
-		background[
-		'red'
-		'brown'
-		'black'
-		'transparet'
-		]
-	},
-
-	scene:[Firstscene]
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 };
 
-const game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
+
