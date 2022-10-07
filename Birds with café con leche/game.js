ee6fda517cc460ceb3d032db90fc318a432b2b88
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2022-10-07 15:41:39 +0200

    pruebas con el bounce, colliders en el world y generación aleatoria de la velocidad de un proyectil

--- Birds with café con leche/game.js
+++ Birds with café con leche/game.js
@@ -8,49 +8,55 @@ preload(){
 
     this.load.image('background', 'images/backgroundd.png');
     this.load.image('puh', 'images/puh.png');
-    this.load.image('platform', 'images/platform.png')
+    this.load.image('platform', 'images/platform.png');
+    this.load.image('skull', 'images/skull.png');
 
 }
 
 create(){
 
-    this.puh = this.physics.add.image(800,50, 'puh');
-    this.puh.setCollideWorldBounds(true);
-    this.puh.body.allowGravity = false;
+    this.physics.world.setBoundsCollision(true,true,true,false);
 
     this.add.image(800,500, 'background');
-    this.platform = this.physics.add.image(400,300, 'platform').setImmovable();
+    this.platform = this.physics.add.image(900,300, 'platform');
+    this.skull = this.physics.add.image(400,100, 'skull');
+    this.puh = this.physics.add.image(400,20, 'puh');
+    this.puh.body.allowGravity = false;
+    this.skull.setBounce(3);
+    let velocity = 100 * Phaser.Math.Between(1.3,2);
+    if(Phaser.Math.Between(0,10)>5){
+        velocity = 0 - velocity;
+    }
+    this.skull.setVelocity(velocity, 10);
+    
+    
+    this.puh.setCollideWorldBounds(true);
+    this.skull.setCollideWorldBounds(true);
     this.platform.body.allowGravity = false;
+    this.physics.add.collider(this.skull, this.platform);
 
-    this.physics.add.collider(this.puh, this.platform);
     this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 update(){
    
     if(this.cursors.left.isDown){
-        this.puh.setVelocityX(-200);
+        this.platform.setVelocityX(-200);
     }
     else if(this.cursors.right.isDown){
-        this.puh.setVelocityX(200);
+        this.platform.setVelocityX(200);
     }
     else{
-        this.puh.setVelocityX(0);
+        this.platform.setVelocityX(0);
     }
     if(this.cursors.up.isDown){
-        this.puh.setVelocityY(-200);
+        this.platform.setVelocityY(-200);
     }
     else if(this.cursors.down.isDown){
-        this.puh.setVelocityY(200)
+        this.platform.setVelocityY(200)
     }
     else{
-        this.puh.setVelocityY(0);
+        this.platform.setVelocityY(0);
+        }
     }
-}
-
-
-
-
-
-
 }
