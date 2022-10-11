7dfdf68b2714abec8be7481db7d55f406ab356fd
PabloM <pabmar31@ucm.es>
2022-10-11 16:43:07 +0200

    Vuelo inplementado(con bool en el input) y troceado de parte del codigo en funciones

--- Birds with café con leche/game.js
+++ Birds with café con leche/game.js
@@ -10,7 +10,7 @@ init(){
 
 preload(){
     this.load.image('background', 'assets/backgroundd.png');
-    this.load.image('puh', 'assets/puh/TemporalPuh.png');
+    this.load.image('puh', 'assets/puh/puh.png');
     this.load.image('platform', 'assets/platform.png');
     this.load.image('floor', 'assets/floor.png');
     this.load.image('bone', 'assets/obstacles/bone.png');
@@ -21,32 +21,14 @@ preload(){
 
 create(){
 
-    this.physics.world.setBoundsCollision(true,true,true, false);
-    this.add.image(800,500, 'background');
-    this.gameover = this.add.image(700, 600, 'gameover');
+    this.physics.world.setBoundsCollision(true,true,true, false); // Define limites del mapa
+    this.add.image(800,500, 'background'); // Imagen fondo
+    this.gameover = this.add.image(700, 600, 'gameover'); 
     this.gameover.visible = false;
 
-    this.scoreText = this.add.text(16,16, 'Points: 0', {
-        fontSize: '40px',
-        fill: '#fff',
-        fontFamily: 'verdana, arial, sans-serif' 
-    });
+    this.initScore();
     
-    const obstacles = this.physics.add.group();
-
-    const obstaclesList = ['bone', 'birdClaw', 'birdSkull']
-
-    const obsGen = () => {
-        const xCoord = Math.random() * 1700
-        let obsGen = obstaclesList[Math.floor(Math.random() * 3)]
-        obstacles.create(xCoord, 10, obsGen)
-    }
-
-    const obsGenLoop = this.time.addEvent({
-        delay: 500,
-        callback: obsGen,
-        loop: true,
-    });
+    this.summonObstacles();
 
     /*this.anims.create({
         key: 'walk',
@@ -57,15 +39,11 @@ create(){
 
     this.puhInit();
     
-
     this.platform = this.physics.add.image(900,500, 'platform').setImmovable(true).setScale(1);
     this.platform.body.allowGravity = false;
 
     this.floor = this.physics.add.image(900,950, 'floor').setImmovable(true).setScale(3);
     this.floor.body.allowGravity = false;
-
-    //this.puhInit();
-
 
     /*let velocity = 100 * Phaser.Math.Between(1.3,2);
 
@@ -89,14 +67,40 @@ create(){
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
     this.cursors = this.input.keyboard.createCursorKeys();     
 }
+
+initScore(){
+    this.scoreText = this.add.text(16,16, 'Points: 0', {
+        fontSize: '40px',
+        fill: '#fff',
+        fontFamily: 'verdana, arial, sans-serif' 
+    });
+}
+
 addScore(){
         this.score++;
         this.scoreText.setText('Points: ' + this.score);
         console.log('1 punto');
+}
+
+summonObstacles(){
+    const obstacles = this.physics.add.group();
+    const obstaclesList = ['bone', 'birdClaw', 'birdSkull']
+
+    const obsGen = () => {
+        const xCoord = Math.random() * 1700
+        let obsGen = obstaclesList[Math.floor(Math.random() * 3)]
+        obstacles.create(xCoord, 10, obsGen)
+    }
+
+    const obsGenLoop = this.time.addEvent({
+        delay: 500,
+        callback: obsGen,
+        loop: true,
+    });
 }
 
 puhInit(){
@@ -107,13 +111,7 @@ puhInit(){
     this.puh.setCollideWorldBounds(true);
 }
 
-gameOver(){
-    this.puh.visible = false;
-    console.log('fin');
-    this.gameover.visible = true;
-}
-
-characterInputManager(){
+characterInputManager(fly = false){
 
     if(this.keyA.isDown){
         this.puh.setVelocityX(-400);
@@ -125,18 +123,38 @@ characterInputManager(){
     else{
         this.puh.setVelocityX(0);
     }
-   if(this.keyW.isDown && this.puh.body.touching.down){
-        this.puh.setVelocityY(-2000)
+    if (!fly){
+        if(this.keyW.isDown && this.puh.body.touching.down){
+            this.puh.setVelocityY(-2000)
+        }
+        else if (this.puh.body.touching.down){ 
+            this.puh.setVelocityY(0);
+        }
     }
-    else if (this.puh.body.touching.down){ 
-        this.puh.setVelocityY(0);
+    else{
+        if(this.keyW.isDown){
+            this.puh.setVelocityY(-500)
+        }
+        else if (this.keyS.isDown){
+            this.puh.setVelocityY(250);
+        }
+        else{ 
+            this.puh.setVelocityY(100);
+        }
     }
 
 }
 
+gameOver(){
+    this.puh.visible = false;
+    console.log('fin');
+    this.gameover.visible = true;
+}
+
+
 update(){
 
-    this.characterInputManager();
+    this.characterInputManager(true);
 
     if(this.keyQ.isDown){
         this.scene.start('game');
