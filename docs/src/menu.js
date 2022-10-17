3b0e7515ca87ff504c687d9f5bf43159b1b1f9a8
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2022-10-17 23:32:31 +0200

    cambios menú, palomas y logo añadidos.

--- docs/src/menu.js
+++ docs/src/menu.js
@@ -16,13 +16,16 @@ export class Menu extends Phaser.Scene {
         this.load.audio('menuMusic', 'assets/audio/menuMusic.mp3');
         this.load.image('text', 'assets/birdstext.png');
         this.load.image('paloma', 'assets/paloma.png');
+        this.load.image('centralperk', 'assets/centralperk.png');
 
     }
 
     create(){
 
         this.add.image(720,900, 'menuFondo').setScale(1.3); // Imagen fondo
-        this.add.image(720, 600, 'paloma').setScale(0.4);
+        this.add.image(720,500, 'centralperk').setScale(0.5); // Imagen fondo
+        this.add.image(220, 1050, 'paloma').setScale(0.4);
+        this.add.image(1250, 1050, 'paloma').setScale(0.4);
         this.add.image(720,150, 'text').setScale(1.3);
         
        
