6a96c5d81f80cfbac375cd8b8ae698f4c3c73843
PabloM <pabmar31@ucm.es>
2022-10-19 18:17:03 +0200

    Clase Puh hecha y solucionado error restart camerafollows

---
+++ docs/src/puh.js
@@ -0,0 +1,29 @@
+export default class Puh extends Phaser.GameObjects.Sprite{
+constructor(scene, x, y){
+    super(scene, x, y, 'puh');
+
+    this.scene.add.existing(this);
+
+    this.scene.anims.create({
+        key: 'iddle',
+        frames: scene.anims.generateFrameNumbers('puhIddle', {start:0, end:3}),
+        frameRate: 5,
+        repeat: -1
+    });
+
+    this.scene.anims.create({
+        key: 'move',
+        frames: scene.anims.generateFrameNumbers('puhIddle', {start:0, end:5}),
+        frameRate: 5,
+        repeat: -1
+    });
+     //if(this.anim.currentAnim.key === 'move')this.play
+
+    this.setScale(3);
+    this.play('iddle');
+}
+
+preUpdate(t, dt){
+    super.preUpdate(t,dt);
+}
+}
