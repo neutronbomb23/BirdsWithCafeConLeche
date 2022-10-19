c798e133bb6be02ea750e6bd996c367e05d0e5e2
PabloM <pabmar31@ucm.es>
2022-10-19 18:32:30 +0200

    m

--- docs/src/puh.js
+++ docs/src/puh.js
@@ -21,9 +21,27 @@ constructor(scene, x, y){
 
     this.setScale(3);
     this.play('iddle');
+
+    this. w = this.scene.input.keyboard.addKey('W');
+    this. s = this.scene.input.keyboard.addKey('S');
+    this. a = this.scene.input.keyboard.addKey('A');
+    this. d = this.scene.input.keyboard.addKey('D');
 }
 
 preUpdate(t, dt){
     super.preUpdate(t,dt);
+
+    if(this.w.isDown){
+        this.y -= 400 * dt/1000;
+    }
+    if(this.s.isDown){
+        this.y += 400 * dt/1000;
+    }
+    if(this.a.isDown){
+        this.x -= 400 * dt/1000;
+    }
+    if(this.d.isDown){
+        this.x += 400 * dt/1000;
+    }
 }
 }
