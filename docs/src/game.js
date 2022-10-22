136c0ea7653c70139862015ea28cf2a7b873f343
PabloM <pabmar31@ucm.es>
2022-10-22 19:29:49 +0200

    eliminados del game metodos de la clase Puh otra vez xDn't

--- docs/src/game.js
+++ docs/src/game.js
@@ -14,222 +14,167 @@ const PUHX = 700; const PUHY = 1500; // Posiciones iniciales de puh
 var startTime; // Runtime en el momento en el que empieza la escena
 
 export class Game extends Phaser.Scene{
-constructor(){
-    super({key: 'game'});
-}
-
-init(){
-    this.score = 0;
-}
-preload(){ // precarga los assets
-    this.load.image('background', 'assets/backgroundd.png');// fondo
-    this.load.spritesheet('puhIddle', 'assets/puh/puh.png', {frameWidth:32,  frameHeight: 32});// idle de Puh
-    this.load.spritesheet('puhMove', 'assets/puh/caminar.png', {frameWidth:32,  frameHeight: 32});// Movimiento de Puh
-    this.load.spritesheet('puhFly', 'assets/puh/Walk.png', {frameWidth:32,  frameHeight: 32});// Vuelo de Puh
-    this.load.image('platform', 'assets/platform.png');// plataforma
-    this.load.image('floor', 'assets/floor.png');// suelo
-    this.load.image('bone', 'assets/obstacles/bone.png');// hueso
-    this.load.image('birdSkull', 'assets/obstacles/birdSkull.png');// calavera de pájaro
-    this.load.image('birdClaw', 'assets/obstacles/birdClaw.png');// garra de pájaro
-    this.load.audio('song','assets/audio/game.mp3');// sonido
-}
-
-create(){
-    startTime = this.time.now;
-    cameraMoves = true;
-    this.lastTimeObbs = 0;
-    this.song = this.sound.add('song');
-    this.song.play();
-
-
-    //this.physics.world.setBoundsCollision(true,true,true, false); // Define limites del mapa
-    this.add.image(720, 410, 'background'); // Imagen fondo
-
-    this.puh = new Puh(this, 600, 1000);// instanciación de Puh
-    //new PuhAn(this, 600, 1000);// instanciación de Puh
-
-
-    this.initScore();
-
-    /*this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNumbers('puh', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });*/
-    
-    this.platform = this.physics.add.image(700,1650, 'platform').setImmovable(true).setScale(1);
-    this.platform.body.allowGravity = false;
-
-    this.floor = this.physics.add.image(720,800, 'floor').setImmovable(true).setScale(1);
-    this.floor.body.allowGravity = false;
-
-    /*let velocity = 100 * Phaser.Math.Between(1.3,2);
-
-    if(Phaser.Math.Between(0,10)>5){
-        velocity = 0 - velocity;
+    constructor(){
+        super({key: 'game'});
     }
-    */
+
+    init(){
+    this.score = 0;
+    }
+
+    preload(){ // precarga los assets
+        this.load.image('background', 'assets/backgroundd.png');// fondo
+        this.load.spritesheet('puhIddle', 'assets/puh/puh.png', {frameWidth:32,  frameHeight: 32});// idle de Puh
+        this.load.spritesheet('puhMove', 'assets/puh/caminar.png', {frameWidth:32,  frameHeight: 32});// Movimiento de Puh
+        this.load.spritesheet('puhFly', 'assets/puh/Walk.png', {frameWidth:32,  frameHeight: 32});// Vuelo de Puh
+        this.load.image('platform', 'assets/platform.png');// plataforma
+        this.load.image('floor', 'assets/floor.png');// suelo
+        this.load.image('bone', 'assets/obstacles/bone.png');// hueso
+        this.load.image('birdSkull', 'assets/obstacles/birdSkull.png');// calavera de pájaro
+        this.load.image('birdClaw', 'assets/obstacles/birdClaw.png');// garra de pájaro
+        this.load.audio('song','assets/audio/game.mp3');// sonido
+    }
+
+    create(){
+        startTime = this.time.now;
+        cameraMoves = true;
+        this.lastTimeObbs = 0;
+        this.song = this.sound.add('song');
+        this.song.play();
+
+
+        //this.physics.world.setBoundsCollision(true,true,true, false); // Define limites del mapa
+        this.add.image(720, 410, 'background'); // Imagen fondo
+
+        this.puh = new Puh(this, 600, 1000);// instanciación de Puh
+
+        this.initScore();
     
-    this.physics.add.collider(this.puh, this.platform);
-    this.physics.add.collider(this.puh, this.floor);
+        this.platform = this.physics.add.image(700,1650, 'platform').setImmovable(true).setScale(1);
+        this.platform.body.allowGravity = false;
 
-    this.platform.setCollideWorldBounds(true);
-    this.platform.body.onWorldBounds=true;
-    this.physics.add.collider(this.platform, this.floor);
-    //this.physics.add.collider(this.birdClaw, this.platform, this.addScore.bind(this), null);
+        this.floor = this.physics.add.image(720,800, 'floor').setImmovable(true).setScale(1);
+        this.floor.body.allowGravity = false;
 
-    this.obstaclesList = ['bone', 'birdClaw', 'birdSkull']
-    this.obstacles = this.physics.add.group();
+        /*let velocity = 100 * Phaser.Math.Between(1.3,2);
 
-    this.physics.add.collider(this.obstacles, this.puh, this.gameOver.bind(this), null);
-    this.physics.add.collider(this.obstacles, this.platform);
+        if(Phaser.Math.Between(0,10)>5){
+            velocity = 0 - velocity;
+        }
+        */
     
-    //CAMARA
-    this.initCamera();
+        this.physics.add.collider(this.puh, this.platform);
+        this.physics.add.collider(this.puh, this.floor);
 
-   /* this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);*/
-    this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-    //this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
- 
-    this.cursors = this.input.keyboard.createCursorKeys();     
-}
+        this.platform.setCollideWorldBounds(true);
+        this.platform.body.onWorldBounds=true;
+        this.physics.add.collider(this.platform, this.floor);
+        //this.physics.add.collider(this.birdClaw, this.platform, this.addScore.bind(this), null);
 
-initScore(){
-    this.scoreText = this.add.text(16,16, 'Points: 0', {
+        this.obstaclesList = ['bone', 'birdClaw', 'birdSkull']
+        this.obstacles = this.physics.add.group();
+
+        this.physics.add.collider(this.obstacles, this.puh, this.gameOver.bind(this), null);
+        this.physics.add.collider(this.obstacles, this.platform);
+    
+        //CAMARA
+        this.initCamera();
+
+        this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        this.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        this.cursors = this.input.keyboard.createCursorKeys();     
+    }
+
+    initScore(){
+        this.scoreText = this.add.text(16,16, 'Points: 0', {
         fontSize: '40px',
         fill: '#fff',
         fontFamily: 'verdana, arial, sans-serif' 
-    });
-}
-
-initCamera(){
-    this.cameras.main.startFollow(this.puh); // Sigue a puh 
-    this.cameras.main.setFollowOffset(0, CAMPOSY); // Distancia entre el la camara y puh
-    this.cameras.main.setLerp(0, 0.1);
-    this.cameras.main.scrollX = false;
-}
-
-initPuh(){
-    this.puh = this.physics.add.image(PUHX, PUHY, 'puh').setImmovable(false).setScale(2);
-    this.puh.setBounce(3);
-    this.puh.body.allowGravity = true;
-
-    //this.puh.setCollideWorldBounds(true);
-}
-
-updateCamera(){
-    var runTimeSecs = (this.time.now - startTime) * 0.001; // Tiempo desde que se inicio la escena
-    if(DEBUG)console.log(runTimeSecs)
-    camCurrentPosY = CAMPOSY + runTimeSecs*CAMERASPEED - (PUHY - this.puh.y); // Distancia entre el la camara y puh
-    this.cameras.main.setFollowOffset(0, camCurrentPosY); // Set de la posición y de la camara
-
-}
-
-addScore(){
+        });
+    }
+    
+    addScore(){
         this.score++;
         this.scoreText.setText('Points: ' + this.score);
         console.log('1 punto');
-}
-/*characterInputManager(fly = false){
+    }
 
-    if(this.keyA.isDown){
-        this.puh.setVelocityX(-400);
+    initCamera(){
+        this.cameras.main.startFollow(this.puh); // Sigue a puh 
+        this.cameras.main.setFollowOffset(0, CAMPOSY); // Distancia entre el la camara y puh
+        this.cameras.main.setLerp(0, 0.1);
+        this.cameras.main.scrollX = false;
     }
-    else if(this.keyD.isDown){
-        this.puh.setVelocityX(400);
-    }
-    else{
-        this.puh.setVelocityX(0);
-    }
-    if (!fly){
-        if(this.keyW.isDown && this.puh.body.touching.down){
-            this.puh.setVelocityY(-2000)
-        }
-        else if (this.puh.body.touching.down){ 
-            this.puh.setVelocityY(0);
-        }
-        else if(this.puh.body.touching.up) this.puh.setVelocityY(0);
-    }
-    else{
-        if(this.keyW.isDown){
-            this.puh.setVelocityY(-1000)
-        }
-        else if (this.keyS.isDown){
-            this.puh.setVelocityY(250);
-        }
-        else{ 
-            this.puh.setVelocityY(100);
-        }
-    }
-    if(this.keyQ.isDown){
-        this.scene.start('game');
-    }
-}*/
 
-gameOver(){
-    this.puh.visible = false;
-    console.log('Puh Abatido');
-    this.song.stop();
-    this.scene.start('GameOver');
-}
-generateObs(){
-    let idObs = this.obstaclesList[Math.floor(Math.random() * 3)]
-    let toni =  new FallingObjects(this, 100, idObs);
-    this.obstacles.add(toni);
+    updateCamera(){
+        var runTimeSecs = (this.time.now - startTime) * 0.001; // Tiempo desde que se inicio la escena
+        if(DEBUG)console.log(runTimeSecs)
+        camCurrentPosY = CAMPOSY + runTimeSecs*CAMERASPEED - (PUHY - this.puh.y); // Distancia entre el la camara y puh
+        this.cameras.main.setFollowOffset(0, camCurrentPosY); // Set de la posición y de la camara
+    }
 
-    //toni.setBounce(2);  
+    gameOver(){
+        this.puh.visible = false;
+        console.log('Puh Abatido');
+        this.song.stop();
+        this.scene.start('GameOver');
+    }   
+
+    generateObs(){
+        let idObs = this.obstaclesList[Math.floor(Math.random() * 3)]
+        let toni =  new FallingObjects(this, 100, idObs);
+        this.obstacles.add(toni);
+
+        //toni.setBounce(2);  
     
-    console.log(idObs);
-}
-stopMusic(){
-    this.song.stop();
-}
-
-update(t,dt){
-
-    this.lastTimeObbs += dt;
-    if(this.lastTimeObbs > 1000)
-    {
-        this.generateObs();
-        console.log(this.lastTimeObbs);
-        this.lastTimeObbs = 0;
+        console.log(idObs);
     }
 
-    if(cameraMoves){
-        this.updateCamera(); // Actualiza su posición respecto a puh
-    }
-    else{
-        this.cameras.main.stopFollow(); // Se queda quieta en el eje Y
-    }
-
-    if(DEBUG){
-    console.log(this.puh.y)
-    }
-
-    if(this.keyQ.isDown){
-        this.scene.restart();
+    stopMusic(){
         this.song.stop();
     }
-    if(this.keyZ.isDown){
-        this.scene.launch('GamePause', {me: this.scene});
-        this.scene.pause();
-    }
 
+    update(t,dt){
+        this.lastTimeObbs += dt;
+        if(this.lastTimeObbs > 1000)
+        {
+            this.generateObs();
+            console.log(this.lastTimeObbs);
+            this.lastTimeObbs = 0;
+        }
+
+        if(cameraMoves){
+            this.updateCamera(); // Actualiza su posición respecto a puh
+        }
+        else{
+            this.cameras.main.stopFollow(); // Se queda quieta en el eje Y
+        }
+
+        if(this.keyQ.isDown){
+            this.scene.restart();
+            this.song.stop();
+        }
+
+        if(this.keyZ.isDown){
+            this.scene.launch('GamePause', {me: this.scene});
+            this.scene.pause();
+        }
 
         if(this.puh.y > 1600) {
             this.gameOver();
     
         }
-        if(this.obstacles.y > 1600)
-        {
+
+        if(this.obstacles.y > 1600){
             this.addScore();
-        }
+        }            
+        
         if (camCurrentPosY > TOP){ // Detiene el seguimiento de camara
             cameraMoves = false;
+        }
+        
+        if(DEBUG){
+            console.log(this.puh.y)
         }
     }
 }
