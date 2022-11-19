2b8077d7304866e9de76c4a3353f4d471219641a
PabloM <pabmar31@ucm.es>
2022-11-19 19:14:17 +0100

    zarzas base

---
+++ docs/src/spike.js
@@ -0,0 +1,13 @@
+export default class Spike extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y){
+        super(scene, x, y, 'spike');
+        this.scene.add.existing(this); 
+        this.scene.physics.add.existing(this);
+        this.body.allowGravity = false;
+
+        this.hit = true;
+
+        this.body.setCollideWorldBounds(true);
+        this.body.setImmovable(true);
+    }
+}
