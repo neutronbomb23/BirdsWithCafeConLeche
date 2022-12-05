580cfc0f0869df8972815fb899790d36fd92f13f
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2022-12-05 12:35:24 +0100

    Código estructurado y bug Boss arreglado
    Había dos bugs en el Boss cuando Puh subía mientras Rick atacaba y cuando Rick lanzaba un gota y Puh bajaba, pero ya están solucionados

--- docs/src/Rick.js
+++ docs/src/Rick.js
@@ -26,7 +26,7 @@ export default class Rick extends Phaser.GameObjects.Sprite{
         });
 
         this.scene.anims.create({
-            key: 'attack',
+            key: 'attackR',
             frames: scene.anims.generateFrameNumbers('RickAttack', {start:0, end:23}),
             frameRate: 5,
             repeat: -1
@@ -65,52 +65,63 @@ export default class Rick extends Phaser.GameObjects.Sprite{
         let distY = Math.abs(this.y - puhPosY);// distancia en valor absoluto entre Puh y Rick
         let range = this.x - puhPosX;// distancia en X entre Puh y Rick
 
-        if(!this.dash && distY < 110) 
-        {
-            if(this.x > 200 && this.x < 1300)this.dash = true;// si no está ya en los extremos
-            if(range > 0) { // puh está a la izquierda
-                this.body.setVelocityX(-900);
-                this.setFlip(true, false);
-                this.bossSound = true;
-            }
-            else { // puh está a la derecha
-                this.body.setVelocityX(900);
-                this.setFlip(false, false);
-                this.bossSound = true;
-            }
-            if(this.anims.currentAnim.key !== 'attack') {// animación
-            this.play('attack');
-            }
-        }
-        else if(this.dash && (this.x <= 200 || this.x >= 1300)) {// daño a Rick
-            if(this.anims.currentAnim.key === 'attack') {
-                this.body.setVelocityX(0);// para a Rick
-                this.play('idleR');// animación
-                this.scene.Damage.resume();//tween de daño
-                if(this.first) {
-                    this.timer = 0;// contador a 0
-                    this.first = false;// booleano a false para no reiniciar el contador a cero
-                }
-            }
-            if(this.timer >= 1000) {
-                this.dash = false;// fin del daño y del ataque
-                this.first = true;// booleano a true para reiniciar el contador en el siguiente ataque
-                this.scene.Damage.pause();// tween pausado
-                this.y = 1250;// para que no se caiga por el cambio de escala
-                this.setScale(8);// vuelve a su escala inicial
-            }
-        }
-        else if(distY >= 110) {
+        if(!this.dash && distY < 110) this.attack(range);// ataque
+
+        else if(this.dash && (this.x <= 200 || this.x >= 1300)) this.Dash();// daño a Rick
+
+        else if(distY >= 110 && !this.dash) {
             this.bossSound = true;
-            if(this.dropTimer >= 5000) {
-                this.timerDrop = 0;// contador para que Rick se quede parado
-                this.scene.RickDrop();// instancia gota hacia arriba
-                this.dropTimer = 0;// contador
-                this.body.setVelocityX(0);
-                this.play('idleR')
-            }
-            else if(!this.dash && this.timerDrop >= 1000)this.walking(); // patrulla   
+            if(this.dropTimer >= 5000) this.ThrowDrop();// lanza gota
+            else if(this.timerDrop >= 1000) this.walking(); // patrulla   
         }
+        else if(this.anims.currentAnim.key !== 'attackR') this.attack(range);// para evitar bug porque no entra en la paturlla al descender Puh
+    }
+
+    attack(range)
+    {
+        if(this.x > 200 && this.x < 1300)this.dash = true;// si no está ya en los extremos
+        if(range > 0) { // puh está a la izquierda
+            this.body.setVelocityX(-900);
+            this.setFlip(true, false);
+            this.bossSound = true;
+        }
+        else { // puh está a la derecha
+            this.body.setVelocityX(900);
+            this.setFlip(false, false);
+            this.bossSound = true;
+        }
+        if(this.anims.currentAnim.key !== 'attackR') {// animación
+            this.play('attackR');
+        }
+    }
+
+    Dash()
+    {
+        if(this.anims.currentAnim.key === 'attackR') {
+            this.body.setVelocityX(0);// para a Rick
+            this.play('idleR');// animación
+            this.scene.Damage.resume();//tween de daño
+            if(this.first) {
+                this.timer = 0;// contador a 0
+                this.first = false;// booleano a false para no reiniciar el contador a cero
+            }
+        }
+        if(this.timer >= 1000) {
+            this.dash = false;// fin del daño y del ataque
+            this.first = true;// booleano a true para reiniciar el contador en el siguiente ataque
+            this.scene.Damage.pause();// tween pausado
+            this.y = 1250;// para que no se caiga por el cambio de escala
+            this.setScale(8);// vuelve a su escala inicial
+        }
+    }
+
+    ThrowDrop()
+    {
+        this.timerDrop = 0;// contador para que Rick se quede parado
+        this.scene.RickDrop();// instancia gota hacia arriba
+        this.dropTimer = 0;// contador para la siguiente gota
+        this.body.setVelocityX(0);
+        this.play('idleR');
     }
 
     walking(){
@@ -136,7 +147,6 @@ export default class Rick extends Phaser.GameObjects.Sprite{
         this.timer += dt;
         this.dropTimer += dt;
         this.timerDrop += dt;
-        //console.log(this.dropTimer);
         super.preUpdate(t,dt);
         this.movementManager();
     }
